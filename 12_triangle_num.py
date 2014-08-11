45d0845ed6f07dbe46a696fce87a6006e2d9b069
Ben Brostoff <ben.brostoff@gmail.com>
2014-08-11 18:35:31 -0400

    Clean up indentation

--- 12_triangle_num.py
+++ 12_triangle_num.py
@@ -1,6 +1,6 @@
 def factors(n):    
-    return set(reduce(list.__add__, 
-                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+  return set(reduce(list.__add__, 
+    ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
 
 def tri_sequence():
   x, counter = 1, 1

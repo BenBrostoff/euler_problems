654e920b94d8ad87aa651d503d668b4e61693d15
Ben Brostoff <ben.brostoff@gmail.com>
2014-08-11 14:52:31 -0400

    Refactor on 9

--- 9_special_pythag_trip.py
+++ 9_special_pythag_trip.py
@@ -5,22 +5,14 @@ def py_check(a, b, c):
     return True
 
 def special_py():
-  a = 0
-  b = 0
-  c = 0
-  while py_check(a, b, c) != True:
-    for b_set in range(1, 1000):
-      b = b_set
-      for a_set in range(1, b + 1):
-        a = a_set
-        print "********"
-        print "THIS IS A"
-        print a
-        print "THIS IS B"
-        print b 
-        c = math.sqrt(a ** 2 + b ** 2)
-        if py_check(a,b,c) == True:
-          return a * b * c
+  a, b, c = 0, 0, 0
+  for b_set in range(1, 1000):
+    b = b_set
+    for a_set in range(1, b + 1):
+      a = a_set
+      c = math.sqrt(a ** 2 + b ** 2)
+      if py_check(a,b,c) == True:
+        return a * b * c
 
 print special_py()
 

a17155e621fd2b62626808d54e31377a8e0b1bcd
Ben Brostoff <ben.brostoff@gmail.com>
2014-08-14 13:09:26 -0400

    Solved 8 in ruby

---
+++ 8_largest_product.rb
@@ -0,0 +1,25 @@
+require_relative '8_raw_data'
+
+def array_creator
+  i, temp, test_ground = 0, [], []
+  NUMBERS.split("").each do |number|
+    break if number == NUMBERS.length - 13
+    i += 1
+    (0..12).to_a.each do |element|
+      temp << NUMBERS[i + element].to_i
+    end
+    test_ground << temp
+    temp = []
+  end
+  return test_ground
+end
+
+def product_test(array)
+  product = []
+  array.each do |series|
+    product <<  series.inject{|sum, n| sum * n}
+  end
+  product.max
+end
+
+p product_test(array_creator)

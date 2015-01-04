64a8e980154a06ea651891cc75fb32ccec0f97e6
Ben Brostoff <ben.brostoff@gmail.com>
2015-01-03 20:18:30 -0500

    Actually make a meaningful test

--- binary_gen/bin_test.py
+++ binary_gen/bin_test.py
@@ -9,10 +9,10 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertEqual(to_bin(324), 101000100)
 
     def test_from_bin(self):
-        self.assertEqual(to_bin(1), 1)
-        self.assertEqual(to_bin(4), 100)
-        self.assertEqual(to_bin(7), 111)
-        self.assertEqual(to_bin(324), 101000100)
+        self.assertEqual(from_bin(1), 1)
+        self.assertEqual(from_bin(100), 4)
+        self.assertEqual(from_bin(111), 7)
+        self.assertEqual(from_bin(101000100), 324)
 
 if __name__ == '__main__':
     unittest.main()
